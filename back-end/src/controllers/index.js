57c68ff5c2e4c2d7822f2d06b533dad31bac1e09
DUNG Dang Duc Bao (VTI.SD2) <dung.dangducbao@vti.com.vn>
2023-11-16 16:39:24 +0700

    BE: fix import not found

--- back-end/src/controllers/index.js
+++ back-end/src/controllers/index.js
@@ -2,9 +2,7 @@ module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
 // New controller generated go here
 module.exports.roleController = require('./role.controller');
-module.exports.sinhVienController = require('./sinhVien.controller');
 module.exports.transactionController = require('./transaction.controller');
-module.exports.schoolController = require('./school.controller');
 module.exports.packServiceController = require('./pack-service.controller');
 module.exports.productController = require('./product.controller');
 module.exports.categoryController = require('./category.controller');
--- back-end/src/validations/index.js
+++ back-end/src/validations/index.js
@@ -2,9 +2,7 @@ module.exports.authValidation = require('./auth.validation');
 module.exports.userValidation = require('./user.validation');
 // New validation generated go here
 module.exports.roleValidation = require('./role.validation');
-module.exports.sinhVienValidation = require('./sinhVien.validation');
 module.exports.transactionValidation = require('./transaction.validation');
-module.exports.schoolValidation = require('./school.validation');
 module.exports.packServiceValidation = require('./pack-service.validation');
 module.exports.productValidation = require('./product.validation');
 module.exports.categoryValidation = require('./category.validation');
